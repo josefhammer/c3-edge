3d400178084990fccd35a6bb2aa933c7d7f6e152
Josef Hammer <josef.hammer@aau.at>
2021-05-07 20:55:42 +0200

    shutdown.sh: Also shutdown Jetsons.

--- shutdown.sh
+++ shutdown.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
-ansible nodes -a "shutdown -h now" --become
+ansible nodes:jetsons -a "shutdown -h now" --become
 sudo shutdown -h now
