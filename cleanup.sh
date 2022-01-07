72c80091bbd2fc0364e2c659c99ec96d9ed8d0ae
Josef Hammer <josef.hammer@aau.at>
2022-01-07 19:11:15 +0100

    Added uniform cleanup scripts for emu-edge + k3s-single + k3s.

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [ $# -eq 0 ]
+  then
+    echo "Usage: $0 <playbook>"
+    echo ""
+    exit 1
+fi
+
+PLAYBOOK=$1
+shift
+
 # Run Provision Playbook
 cd $DIR  # ansible.cfg is only read from the _current_ working directory
-ansible-playbook emu.yml --extra-vars "emu_cleanup=true" "$@"
+ansible-playbook "$PLAYBOOK" --extra-vars "cleanup=true" "$@"
